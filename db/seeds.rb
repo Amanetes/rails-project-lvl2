5e19ef6a7f99d3daa8638f2e357be828e37f39f0
Sergey <ser_8904@mail.ru>
2022-05-31 18:19:02 +0300

    update locales

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,17 +8,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# 5.times do
-#   Category.create(
-#     name: Faker::Movie.unique.title
-#   )
-# end
+5.times do
+  Category.create(
+    name: Faker::Movie.unique.title
+  )
+end
 
-# 5.times do
-#   Post.create(
-#     title: Faker::Movies::StarWars.unique.droid,
-#     body: Faker::Movies::StarWars.wookiee_sentence,
-#     category_id: 2,
-#     user_id: 1
-#   )
-# end
